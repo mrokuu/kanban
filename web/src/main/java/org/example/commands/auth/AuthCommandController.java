ab445d53a8e580ecebccb284ebb07f3b4a577b19
mrokuu <mroczkowskiadammm@gmail.com>
2024-05-03 08:09:01 +0200

    add empty controller methods

--- web/src/main/java/org/example/commands/auth/AuthCommandController.java
+++ web/src/main/java/org/example/commands/auth/AuthCommandController.java
@@ -1,4 +1,27 @@
 package org.example.commands.auth;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public class AuthCommandController {
+
+    @PostMapping("/signup")
+    public ResponseEntity<Boolean> signUp() {
+      return null;
+    }
+
+    @PostMapping("/loginGoogle")
+    public String loginWithGoogle() throws Exception {
+        return null;
+
+    }
+
+    @PostMapping("/loginFacebook")
+    public String loginWithFacebook() throws Exception {
+        return null;
+    }
+
 }
--- web/src/main/java/org/example/query/auth/AuthQueryController.java
+++ web/src/main/java/org/example/query/auth/AuthQueryController.java
@@ -1,4 +1,10 @@
 package org.example.query.auth;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class AuthQueryController {
+    @GetMapping("/isEmailFree/{email}")
+    public boolean isEmailFree() {
+        return false;
+    }
 }
